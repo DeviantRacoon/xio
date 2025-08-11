03850f97c2b75e090cccd14c31fcd0d53e9a1909
JHectorZ <jzavala@voices.com.mx>
2025-08-10 19:09:15 -0700

    init

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,24 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if os.environ.get("USE_SQLITE", "0") == "1":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mysite",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": "mysite",
-            "USER": "root",
-            "PASSWORD": "root",
-            "HOST": "localhost",
-            "PORT": "3306",
-        }
-    }
+}
 
 
 # Password validation
